d6c935e108460c0f1445eabe6a3eac1ac6c7827f
jinsumin <suminjin@naver.com>
2020-07-29 11:54:12 +0900

    send message client to backend

--- MobileApp/screens/HomeScreen.js
+++ MobileApp/screens/HomeScreen.js
@@ -1,19 +1,29 @@
 import { StatusBar } from "expo-status-bar";
-import React, { useEffect } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useEffect, useState, useRef } from "react";
+import { StyleSheet, Text, View, TextInput } from "react-native";
 import io from "socket.io-client";
 
 export default function HomeScreen() {
-  useEffect(function () {
-    io("http://172.30.1.43:3001");
+  const [messageToSend, setMessageToSend] = useState("");
+  const socket = useRef(null);
+  
+  useEffect(() => {
+    socket.current = io("http://192.168.10.194:3002");
   }, []);
+
+  const sendMessage = () => {
+    socket.current.emit("message", messageToSend);
+  }
 
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Hello React Native!</Text>
+      <TextInput
+        value={messageToSend}
+        onChangeText={(text) => setMessageToSend(text)}
+        placeholder="Enter chat message.."
+        onSubmitEditing={sendMessage}
+      />
     </View>
   );
 }
--- socket.io-backend/server.js
+++ socket.io-backend/server.js
@@ -1,9 +1,10 @@
 const io = require("socket.io")();
 
-io.on("connection", function () {
+io.on("connection", socket => {
     console.log("a user connected!");
-    console.log("a user connected!");
-    console.log("a user connected!");
+    socket.on("message", message => {
+        console.log(message);
+    })
 });
 
-io.listen(3001);
+io.listen(3002);
