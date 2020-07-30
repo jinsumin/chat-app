1672bde07d8a6fea77494eabc5de1988a8347994
jinsumin <suminjin@naver.com>
2020-07-30 11:24:56 +0900

    random avatar

--- socket.io-backend/server.js
+++ socket.io-backend/server.js
@@ -2,15 +2,22 @@ const io = require("socket.io")();
 const messageHandler = require("./handlers/message.handler");
 
 let currentUserId = 2;
-const userIds = {};
+const users = {};
+
+function createUserAvatarUrl() {
+  const rand1 = Math.round(Math.random() * 200 + 100);
+  const rand2 = Math.round(Math.random() * 200 + 100);
+  return `https://placeimg.com/${rand1}/${rand2}/any`;
+}
 
 io.on("connection", socket => {
   console.log("a user connected!");
   console.log(socket.id);
-  userIds[socket.id] = { userId: currentUserId++ };
+  users[socket.id] = { userId: currentUserId++ };
   socket.on("join", username => {
-    userIds[socket.id].username = username;
-    messageHandler.handleMessage(socket, userIds);
+    users[socket.id].username = username;
+    users[socket.id].avatar = createUserAvatarUrl();
+    messageHandler.handleMessage(socket, users);
   });
 });
 
