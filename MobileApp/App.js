0ac80f582dbe501f736bec7ca2165fb000f4d8c5
jinsumin <suminjin@naver.com>
2020-07-30 16:28:04 +0900

    add user avatar and touchableOpacity to friendListScreen

--- MobileApp/App.js
+++ MobileApp/App.js
@@ -12,6 +12,8 @@ function reducer(state = {}, action) {
   switch (action.type) {
     case "message":
       return { ...state, message: action.data };
+    case "users_online":
+      return { ...state, usersOnline: action.data };
     default:
       return state;
   }
---
+++ MobileApp/screens/FriendListScreen.js
@@ -0,0 +1,49 @@
+import React from "react";
+import {
+  View,
+  Text,
+  Image,
+  FlatList,
+  StyleSheet,
+  TouchableOpacity,
+} from "react-native";
+import { useSelector } from "react-redux";
+
+export default function FriendListScreen() {
+  const usersOnline = useSelector((state) => state.usersOnline);
+  console.log("usersOnline", usersOnline);
+
+  const { itemContainerStyle, avatarImageStyle, avatarNameViewStyle } = styles;
+
+  return (
+    <View style={{ flex: 1 }}>
+      <FlatList
+        data={usersOnline}
+        renderItem={({ item }) => {
+          console.log("item", item);
+          return (
+            <TouchableOpacity>
+              <View style={itemContainerStyle}>
+                <Image style={avatarImageStyle} source={{ uri: item.avatar }} />
+                <View style={avatarNameViewStyle}>
+                  <Text style={{ fontSize: 20 }}>{item.username}</Text>
+                </View>
+              </View>
+            </TouchableOpacity>
+          );
+        }}
+        keyExtractor={(item) => item.userId}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  itemContainerStyle: { flex: 1, flexDirection: "row" },
+  avatarImageStyle: { width: 100, height: 100, borderRadius: 50 },
+  avatarNameViewStyle: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
